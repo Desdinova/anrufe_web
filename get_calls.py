602712a6b182fb3604e90dd664d3212d7c3636cb
Desdinova <florian@infra.net>
2018-10-09 17:28:43 +0200

    added code to get_calls.py

--- get_calls.py
+++ get_calls.py
@@ -1,1 +1,34 @@
- 
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-# enable debugging
+
+import cgitb, MySQLdb, datetime
+# cgitb for Apache cgi connector
+# MySQLdb for MySQL connection
+# datetime for dates
+
+# Enable Apache cgi connector
+cgitb.enable()
+
+# Header for Webpage
+print("Content-Type: text/html;charset=utf-8")
+print()		# doesn't work without this
+
+# Variables
+today = datetime.datetime.now()		# time right now
+dif = datetime.timedelta(days=5)
+daysago = today - dif
+
+# Connection to MySQL
+db = MySQLdb.connect(host="localhost",  # your host 
+                     user="root",       # username
+                     passwd="",     # password
+                     db="asterisk")   # name of the database
+
+# Cursor to execute queries
+cur = db.cursor()
+
+# Query cdr
+cur.execute("SELECT calldate, clid, src, dst from cdr WHERE calldate >=" + "'" + str(daysago) + "'")
+
+for a in cur.fetchall():
+	print (a[0], " ", a[1], " ", a[2], "->", a[3], "<br>")
